3bddece3ca517660fcf8649ca9262b92784241ca
dingpeng24001 <dingpeng24001@talkweb.com.cn>
2022-04-15 14:03:19 +0800

    update dockerfile

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@
 
 export NAMESPACE=wutong-adaptor
 export VERSION=v1.0.0-stable
-docker buildx create --use --name adaptorbuilder
-docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile . 
-docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile . 
-docker buildx rm adaptorbuilder
+# docker buildx create --use --name adaptorbuilder
+docker buildx use adaptorbuilder
+docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
+docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile.multiarch . 
+# docker buildx rm adaptorbuilder
