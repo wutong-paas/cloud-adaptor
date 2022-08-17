ae5bfe0fcd751971db4c9684d097fecaff90a8de
dp <dp.io@outlook.com>
2022-08-17 13:56:03 +0800

    update release scripts

--- amd64-release.sh
+++ amd64-release.sh
@@ -1,4 +1,8 @@
-go mod download
-go build -o ./bin/cloud-adaptor ./cmd/cloud-adaptor
+#! /bin/bash
+
+export NAMESPACE=wutong-adaptor
+export VERSION=v1.0.0-stable
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/cloud-adaptor ./cmd/cloud-adaptor
 docker build . -t swr.cn-southwest-2.myhuaweicloud.com/wutong/wutong-adaptor:v1.0.0-stable
 docker push swr.cn-southwest-2.myhuaweicloud.com/wutong/wutong-adaptor:v1.0.0-stable
