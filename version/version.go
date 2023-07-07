25ae70ac16e920061af635fbdf3288d1a87d9c9c
poneding <poneding@gmail.com>
2023-07-07 16:20:47 +0800

    init v1.3.0 version

--- version/version.go
+++ version/version.go
@@ -24,10 +24,10 @@ import (
 )
 
 // WutongRegionVersion wutong region install version
-var WutongRegionVersion = "v1.2.0-stable"
+var WutongRegionVersion = "v1.3.0"
 
 // OperatorVersion operator image tag
-var OperatorVersion = "v1.2.0-stable"
+var OperatorVersion = "v1.3.0"
 
 // InstallImageRepo install image repo
 var InstallImageRepo = "swr.cn-southwest-2.myhuaweicloud.com/wutong"
