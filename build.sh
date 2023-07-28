ec97daba422fb6d1bd3c1f53e73751546ae0c3f2
poneding <poneding@gmail.com>
2023-07-28 22:11:02 +0800

    update ci and build script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-export IMAGE_REGISTRY=swr.cn-southwest-2.myhuaweicloud.com/wutong
-export IMAGE_NAME=wutong-adaptor
-export VERSION=v1.4.0
+WUTONG_REGISTRY=${WUTONG_REGISTRY:-'swr.cn-southwest-2.myhuaweicloud.com/wutong'}
+IMAGE_NAME=wutong-adaptor
+VERSION=v1.4.0
 
 go mod download
 
@@ -10,7 +10,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o bin/amd64/cl
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 GO111MODULE=on go build -a -o bin/arm64/cloud-adaptor ./cmd/cloud-adaptor
 
 docker buildx use gobuilder || docker buildx create --use --name gobuilder
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ${IMAGE_REGISTRY}/${IMAGE_NAME}:${VERSION} -f Dockerfile.local . 
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ${WUTONG_REGISTRY}/${IMAGE_NAME}:${VERSION} -f Dockerfile.local . 
 # docker buildx rm gobuilder
 
 rm -rf ./bin/*
